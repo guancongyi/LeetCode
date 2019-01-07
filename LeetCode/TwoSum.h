32fd2b5fe31be812128e5e63e7b2b10823cbde8e
guancongyi <congyiguan@gmail.com>
2019-01-07 09:55:34 -0500

    two sum 12 ms

--- LeetCode/TwoSum.h
+++ LeetCode/TwoSum.h
@@ -1,3 +1,41 @@
+#include<vector>
+#include<map>
 
-#pragma once
-//Two Sum
+
+using namespace std;
+
+class SolutionTwoSum {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		int cap = nums.size();
+		map<int, int> dict;
+		map<int, int> lut;
+		vector<int> ret;
+		vector<int> rep;
+		
+		for (int i = 0; i < cap; i++) {
+			int val = nums[i];
+			int id = i;
+			lut[val] = id;
+			
+			int diff = target - val;
+
+			if (diff == val) {
+				if (dict[val] == 1) {
+					rep.push_back(id);
+					return rep;
+				}
+				else {
+					rep.push_back(id);
+				}
+			}
+			else if(dict[diff] == 1){
+				ret.push_back(lut[diff]);
+				ret.push_back(id);
+				return ret;
+			}
+			dict[val] = 1;
+		}
+		return ret;
+	}
+};
--- LeetCode/main.cpp
+++ LeetCode/main.cpp
@@ -1,1 +1,24 @@
-//test
+#include<iostream>
+#include"TwoSum.h"
+
+using namespace std;
+
+int main() {
+	
+	SolutionTwoSum s;
+	vector<int> test, rslt;
+	test.push_back(3);
+	test.push_back(3);
+	test.push_back(4);
+
+
+
+	rslt = s.twoSum(test, 6);
+
+	cout << "Solutions are: ";
+	for (int i = 0; i < rslt.size(); i++) {
+		cout << rslt[i] << "   ";
+	}
+
+	return 0;
+}
