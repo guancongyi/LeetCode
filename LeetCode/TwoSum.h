e821cc212e714f1958f9a61cdf5120d07e695710
guancongyi <congyiguan@gmail.com>
2019-05-28 09:34:19 -0400

    test

--- LeetCode/TwoSum.h
+++ LeetCode/TwoSum.h
@@ -4,6 +4,11 @@
 
 using namespace std;
 
+/*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+*/
+
 class SolutionTwoSum {
 public:
 	vector<int> twoSum(vector<int>& nums, int target) {
