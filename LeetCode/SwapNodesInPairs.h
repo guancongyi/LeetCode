6dc071028ebf143e6282676a535c9c4eae6e7072
guancongyi <congyiguan@gmail.com>
2019-05-29 10:52:34 -0400

    added swap node for testing git sync on different devices

---
+++ LeetCode/SwapNodesInPairs.h
@@ -0,0 +1,22 @@
+#pragma once
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+	ListNode* swapPairs(ListNode* head) {
+		
+	}
+};
