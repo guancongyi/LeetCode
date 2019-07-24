bd4ab22e2f2b6a8162c51d90b19662bf8bba171d
guancongyi <congyiguan@gmail.com>
2019-07-24 21:44:04 +0800

    improved rectoverlap

--- LeetCode/RectOverlap.h
+++ LeetCode/RectOverlap.h
@@ -6,7 +6,7 @@ there are 4 cases in total, overlap from four corners...
 
 class RectOverlapSolution {
 public:
-	bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+	bool isRectangleOverlap1(vector<int>& rec1, vector<int>& rec2) {
 		int xLB = rec2[0], xRB = rec2[2];
 		int yLB = rec2[1], yRB = rec2[1];
 		int yLT = rec2[3], yRT = rec2[3];
@@ -24,6 +24,14 @@ public:
 		if ((rec1[1] > rec2[0] && rec1[1] < rec2[2]) && (rec1[2] > rec2[1] && rec1[2] < rec2[3])) { return true; }
 
 		return false;
+
+	}
+
+	bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+		int x1 = rec1[0], y1 = rec1[1], x2 = rec1[2], y2 = rec1[3];
+		int x3 = rec2[0], y3 = rec2[1], x4 = rec2[2], y4 = rec2[3];
+
+		return x1<x4 & x2 > x3 & y1 < y4 & y2 >y3;
 
 	}
 };
